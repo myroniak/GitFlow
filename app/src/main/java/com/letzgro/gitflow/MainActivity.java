018fe736bd1f07b8c9659531b574719289d604e0
roman <roman.myroniak@lezgro.com>
2018-03-06 17:14:37 +0200

    added feature

--- app/src/main/java/com/letzgro/gitflow/MainActivity.java
+++ app/src/main/java/com/letzgro/gitflow/MainActivity.java
@@ -2,12 +2,18 @@ package com.letzgro.gitflow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        textView.setText("Feature 1");
     }
 }
